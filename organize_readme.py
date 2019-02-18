f796d343e8cce95a12554b382be99876d7c8db07
Christopher George <cmgeorge@andrew.cmu.edu>
2019-02-18 18:46:22 -0500

    added code

--- organize_readme.py
+++ organize_readme.py
@@ -7,11 +7,14 @@ directory = os.listdir(directory_path)
 
 new_read_me = directory_path + "README.md"
 text = ""
+i = 0
 for file in directory:
     if (".jpeg" in file):
-        text += '<p align="center">  <img src="'+file+'?"> </p>'
-        text += '<p align="center">'+file+'</p>'
+        i += 1
+        text += '<p align="center">  <img src="'+str(i)+".jpeg"+'?"> </p>'
+        text += '<p align="center">'+str(i)+".jpeg"+'</p>'
         text += "\n\n***\n\n"
+
 print(text)
 file = open(new_read_me, "w")
 file.write(text)
