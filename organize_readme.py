ee11e09b2cd1f98c2314b5c63aaf20632f434c92
Christopher George <cmgeorge@andrew.cmu.edu>
2019-02-18 20:16:34 -0500

    added video

--- organize_readme.py
+++ organize_readme.py
@@ -1,7 +1,7 @@
 import os
 
 
-directory_path = "./intermediate_results/"
+directory_path = "./mixed4b_3x3_bottleneck_pre_relu/"
 
 directory = os.listdir(directory_path)
 
@@ -10,13 +10,13 @@ text = ""
 i = 0
 for file in directory:
     if (".jpeg" in file):
-        # i += 1
-        # text += '<p align="center">  <img src="'+str(i)+".jpeg"+'?"> </p>'
-        # text += '<p align="center">'+str(i)+".jpeg"+'</p>'
-        # text += "\n\n***\n\n"
-        text += '<p align="center">  <img src="'+file+'?"> </p>'
-        text += '<p align="center">'+file+'</p>'
+        i += 1
+        text += '<p align="center">  <img src="'+str(i)+".jpeg"+'?"> </p>'
+        text += '<p align="center">'+str(i)+".jpeg"+'</p>'
         text += "\n\n***\n\n"
+        # text += '<p align="center"><img src="'+file+'?"></p>'
+        # text += '<p align="center">'+file+'</p>'
+        # text += "\n\n***\n\n"
 
 print(text)
 file = open(new_read_me, "w")
